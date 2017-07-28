680aca97a2c62c48d3816cca7b9e26b0455752a5
diglesia <diglesia@google.com>
2017-07-28 16:54:25 -0700

    Add authentication logic to MainActivity

--- app/src/main/java/com/diglesia/hw2017mobiledev/newsapp/MainActivity.java
+++ app/src/main/java/com/diglesia/hw2017mobiledev/newsapp/MainActivity.java
@@ -1,54 +1,96 @@
 package com.diglesia.hw2017mobiledev.newsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager mViewPager;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_browse:
-                    mViewPager.setCurrentItem(0);
-                    setTitle(R.string.title_browse);
-                    return true;
-                case R.id.navigation_myarticles:
-                    mViewPager.setCurrentItem(1);
-                    setTitle(R.string.title_myarticles);
-                    return true;
-                case R.id.navigation_feed:
-                    mViewPager.setCurrentItem(2);
-                    setTitle(R.string.title_feed);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private static final String TAG = "NewsAppMainActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        mViewPager = (ViewPager) findViewById(R.id.content_view_pager);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+                        // be available.
+                        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+                        Toast.makeText(MainActivity.this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // ViewPager + BottomBarNavigation setup
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.content_view_pager);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_browse:
+                        viewPager.setCurrentItem(0);
+                        setTitle(R.string.title_browse);
+                        return true;
+                    case R.id.navigation_myarticles:
+                        viewPager.setCurrentItem(1);
+                        setTitle(R.string.title_myarticles);
+                        return true;
+                    case R.id.navigation_feed:
+                        viewPager.setCurrentItem(2);
+                        setTitle(R.string.title_feed);
+                        return true;
+                }
+                return false;
+            }
+        });
+
         FragmentManager fm = getSupportFragmentManager();
-        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+        viewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
             @Override
             public Fragment getItem(int position) {
                 if (position == 0) {
@@ -65,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
                 return 3;
             }
         });
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
 
@@ -83,5 +125,108 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageScrollStateChanged(int state) {}
         });
+    }
+
+    // Menu setup
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_signin, menu);
+
+        // set title of the menu item, based on log in/out state.
+        MenuItem item = menu.findItem(R.id.action_account);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        item.setTitle(currentUser == null ? "sign in" : "sign out");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_account) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null) {
+                signOut();
+            } else {
+                signIn();
+            }
+            invalidateOptionsMenu(); // reload the menu to update title to new state
+            return true;
+        }
+
+        // This will never be hit, since there's only one menu item.
+        return false;
+    }
+
+    // LOGIN/LOGOUT code
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                updateUI(null);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    // refresh the menu item, the text view, show a toast, and hide the spinner
+    // If user == null, then the user is logged out. If user != null, user is signed in.
+    private void updateUI(FirebaseUser user) {
+        invalidateOptionsMenu(); // This reloads the options menu items.
+        String statusText = (user == null) ? "Signed out" : "Signed in as " + user.getEmail();
+        Toast.makeText(MainActivity.this, statusText , Toast.LENGTH_SHORT).show();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                    }
+                });
     }
 }
